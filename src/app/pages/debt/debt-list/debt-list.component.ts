a3a0e211c6ccad47f4bad9409b4e6d3696fd7372
Anna.Romanczuk <anna.romanczuk@proveedores.boldt.com.ar>
2023-09-21 23:11:44 -0300

    develop: list de préstamos en proceso

--- src/app/pages/debt/debt-list/debt-list.component.ts
+++ src/app/pages/debt/debt-list/debt-list.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Client } from '@app/interfaces/client';
+import { ClientService } from '@app/services/client.service';
 
 @Component({
   selector: 'app-debt-list',
   templateUrl: './debt-list.component.html',
   styleUrls: ['./debt-list.component.scss']
 })
-export class DebtListComponent {
+export class DebtListComponent implements OnInit {
+
+  
+  clients: Client[] = [];
+
+  constructor(private clientService: ClientService){
+
+  }
+
+  ngOnInit(): void {
+    this.clientService.getClients().subscribe(clientsFirebase=>{
+      this.clients = clientsFirebase;
+      console.log('clientsFirebase',clientsFirebase)
+    })
+  }
+
+  async onClickDelete(client: Client){
+    const response = await this.clientService.deleteClient(client);
+    console.log('borrado',response)
+  }
+
 
 }
